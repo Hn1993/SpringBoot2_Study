b8aee6e3b3d260a3ca9dc1c99683a25eeefdfe25
huangan <1274246874@qq.com>
2021-01-23 16:18:50 +0800

    1添加devtools

--- src/main/java/com/an/bean/Car.java
+++ src/main/java/com/an/bean/Car.java
@@ -1,35 +1,20 @@
 package com.an.bean;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
+@NoArgsConstructor //lombok 无参构造器
+@AllArgsConstructor //lombok 生成全参构造器
+@ToString // lombok 生成tostring
+@Data //lombok 自动生成get set
 @Component //手动装配到springboot 或使用  @EnableConfigurationProperties(Car.class)
 @ConfigurationProperties(prefix = "mycar")
 public class Car {
     private String brand;
     private String price;
 
-    public String getBrand() {
-        return brand;
-    }
-
-    public void setBrand(String brand) {
-        this.brand = brand;
-    }
-
-    public String getPrice() {
-        return price;
-    }
-
-    public void setPrice(String price) {
-        this.price = price;
-    }
-
-    @Override
-    public String toString() {
-        return "Car{" +
-                "brand='" + brand + '\'' +
-                ", price='" + price + '\'' +
-                '}';
-    }
 }
--- src/main/java/com/an/controll/HelloController.java
+++ src/main/java/com/an/controll/HelloController.java
@@ -1,14 +1,17 @@
 package com.an.controll;
 
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 public class HelloController {
 
     @RequestMapping("/hello")
     public String helloWord(){
-
+        log.info("hello 请求");
         return "hello,spring boot2";
     }
 }
