e7391a1b44beb1512759dbbfb9a585db34f403eb
huangan <1274246874@qq.com>
2021-03-17 14:15:34 +0800

    添加带参请求方式

--- jpa-demo/src/main/java/jpa/demo/controller/JpaController.java
+++ jpa-demo/src/main/java/jpa/demo/controller/JpaController.java
@@ -6,8 +6,7 @@ import jpa.demo.service.UserService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 
 @RestController
@@ -22,5 +21,22 @@ public class JpaController {
         User user = userService.findByAge(19);
         LOG.info("user="+user.toString());
         return user.getName();
+    }
+
+    @RequestMapping(path = "/getUser/{userName}",method = RequestMethod.GET)
+    public User getUser(@PathVariable String userName){
+        User user = userService.findSql(userName);
+        LOG.info("user="+user.toString());
+        return user;
+    }
+
+    @GetMapping(path = "getUserByName",params = "userName")
+    public User getUserByName(String userName){
+        User user = userService.findSql(userName);
+        LOG.info("user="+user.toString());
+        if (user != null){
+            return user;
+        }
+        return null;
     }
 }
