20b01e01bc3db3a8ae9df91a2d41ab5a34597e69
huangan <1274246874@qq.com>
2021-03-18 16:19:03 +0800

    添加/public/login方法

--- jpa-demo/src/main/java/jpa/demo/controller/PublicController.java
+++ jpa-demo/src/main/java/jpa/demo/controller/PublicController.java
@@ -1,6 +1,13 @@
 package jpa.demo.controller;
 
 import jpa.demo.entity.Result;
+import jpa.demo.entity.User;
+import jpa.demo.service.TokenService;
+import jpa.demo.service.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +18,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController()
 @RequestMapping("/public")
 public class PublicController {
+    private static final Logger LOG = LoggerFactory.getLogger(PublicController.class);
+
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private TokenService tokenService;
 
     @PostMapping(path = "/test")
     public Result publicTest(){
@@ -23,5 +37,24 @@ public class PublicController {
         int i = 1;
 //        i = i / 0;
         return Result.Success("publicTest","ojbk");
+    }
+
+
+    @GetMapping(path = "/login", params = "account")
+    public Result login(String account){
+        User user = userService.findByAccount(account);
+        if (user != null){
+            user.setPwd("");
+
+            LOG.info("user="+user.toString());
+            String token =  tokenService.createToken(user);
+            LOG.info("token = " + token);
+            user.setToken(token);
+            boolean tokenVerify = tokenService.verifyToken(token);
+            LOG.info("tokenVerify = " + tokenVerify);
+
+            return Result.Success("登陆成功!",user);
+        }
+        return Result.Error("登陆失败!");
     }
 }
